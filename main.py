e94094e079bd25b3871079e15e6dfdda3b4f76ec
RachitSaini2108 <69894217+RachitSaini2108@users.noreply.github.com>
2020-10-06 15:58:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.button import Button
+
+
+class TestApp(App):
+    def build(self):
+        return Button(text='Hello World')
+
+    import socket
+    Server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    Server.connect("192.168.1.4", 2108)
+    Server.send("Hello Bitch".encode('utf-8'))
+
+
+TestApp().run()
