7388a5754e790d6eaa8c4ada43aa3f3b3c140add
RachitSaini2108 <69894217+RachitSaini2108@users.noreply.github.com>
2020-10-10 00:12:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ class TestApp(App):
         return Button(text='Hello World')
 
     import socket
+    import pyttsx3
+    engine = pyttsx3.init()
     Server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         Server.connect(("192.168.1.4", 2108))
         Server.send("Hello Bitch".encode('utf-8'))
     except Exception as e:
         print(e)
+    while True:
+        messageToSpeak = Server.recv(1024).decode('utf-8')
+        engine.say(messageToSpeak)
+        engine.runAndWait()
 
 TestApp().run()
-
